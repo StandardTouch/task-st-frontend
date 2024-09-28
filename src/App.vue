4ba65665d17a3db1b8d7fe55aef81862c9a7e924
yaseeen96 <syedyaseen1539@gmail.com>
2024-09-28 12:05:25 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex h-screen w-screen justify-center items-center">
+    <h1 class="text-3xl font-bold">Task Management App First Screen</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

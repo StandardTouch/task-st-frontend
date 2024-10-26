923d018f7ed75ce13c3fe2f7b31b134a5a21e62d
yaseeen96 <syedyaseen1539@gmail.com>
2024-10-26 11:53:04 +0530

    add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,25 @@ export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#45a834',
+          light: '#66c156', // Lighter shade of primary
+          dark: '#358627',  // Darker shade of primary
+          100: '#e2f3df',  // Lightest shade of primary
+          200: '#b4e0a6',  // Lighter variant
+          300: '#87cd70',  // Regular light
+          400: '#5dbb48',  // Slightly lighter than default
+          500: '#45a834',  // Default
+          600: '#358627',  // Darker variant
+          700: '#29661c',  // Darkest shade
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
 }
-

24295b72b690f79aee83bef145e1818de234a7d1
yaseeen96 <syedyaseen1539@gmail.com>
2024-10-26 13:10:19 +0530

    tailwind purge fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'], // updated 'purge' to 'content'
   theme: {
     extend: {
       colors: {
@@ -9,19 +8,17 @@ export default {
           DEFAULT: '#45a834',
           light: '#66c156', // Lighter shade of primary
           dark: '#358627',  // Darker shade of primary
-          100: '#e2f3df',  // Lightest shade of primary
-          200: '#b4e0a6',  // Lighter variant
-          300: '#87cd70',  // Regular light
-          400: '#5dbb48',  // Slightly lighter than default
-          500: '#45a834',  // Default
-          600: '#358627',  // Darker variant
-          700: '#29661c',  // Darkest shade
+          100: '#e2f3df',   // Lightest shade of primary
+          200: '#b4e0a6',   // Lighter variant
+          300: '#87cd70',   // Regular light
+          400: '#5dbb48',   // Slightly lighter than default
+          500: '#45a834',   // Default
+          600: '#358627',   // Darker variant
+          700: '#29661c',   // Darkest shade
         },
       },
     },
   },
-  variants: {
-    extend: {},
-  },
+  darkMode: 'media', // change `false` to `media` or remove the line entirely if not needed
   plugins: [],
 }

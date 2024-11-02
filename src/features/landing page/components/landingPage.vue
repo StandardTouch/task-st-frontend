4c0257828f19fb2fd54ec0a3ed7f3be5c765972a
yaseeen96 <syedyaseen1539@gmail.com>
2024-11-02 13:44:42 +0530

    rename component1 -> landingPage

---
+++ src/features/landing page/components/landingPage.vue
@@ -0,0 +1,9 @@
+<script setup>
+import FooterComponent from "./footerComponent.vue";
+import HeaderComponent from "./headerComponent.vue";
+</script>
+
+<template>
+  <HeaderComponent />
+  <FooterComponent />
+</template>

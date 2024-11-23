a4044e88fb9f4506cc709a18ad153c41b853dbeb
yaseeen96 <syedyaseen1539@gmail.com>
2024-11-23 12:29:54 +0530

    add shadcn

--- src/features/landing page/components/landingPage.vue
+++ src/features/landing page/components/landingPage.vue
@@ -1,13 +1,12 @@
 <script setup>
-
+import { Button } from "@/components/ui/button";
 </script>
 
 <template>
   <div class="flex h-screen w-screen justify-center items-center">
     <h1 class="text-3xl font-bold">Task Management App First Screen</h1>
+    <Button>Click me</Button>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>

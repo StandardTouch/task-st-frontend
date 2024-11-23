039844297a7c19af64405ef3a7c22fa8f9b753e6
nazhus <143502377+nazhus@users.noreply.github.com>
2024-11-23 13:42:39 +0530

    add proper layout

--- src/features/landing page/components/landingPage.vue
+++ src/features/landing page/components/landingPage.vue
@@ -1,9 +1,12 @@
 <script setup>
-import FooterComponent from "./footerComponent.vue";
-import HeaderComponent from "./headerComponent.vue";
+import FooterComponent from "./Footer.vue";
+import HeaderComponent from "./Header.vue";
+import Component2 from "./Component2.vue";
 </script>
 
 <template>
   <HeaderComponent />
+  <Component2/>
+  
   <FooterComponent />
 </template>

7aed2d4b2ebb90555eb944071a3efaea0c6c83d4
yaseeen96 <syedyaseen1539@gmail.com>
2024-11-30 11:53:37 +0530

    add vue-router

--- src/features/landing page/components/component1.vue
+++ src/features/landing page/components/component1.vue
@@ -1,16 +1,13 @@
 <template>
-    <div id="sss">
-      <h1>ssssss</h1>
-    </div>
-  </template>
-  
+  <div id="sss">
+    <h1>ssssss</h1>
+  </div>
+</template>
 
-  
-  <style scoped>
-  #sss {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    text-align: center;
-    margin-top: 60px;
-  }
-  </style>
-  
+<style scoped>
+#sss {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>

43d689b922a2b721d5e072a55e8e6b943f9cc756
yaseeen96 <syedyaseen1539@gmail.com>
2024-11-30 12:11:08 +0530

    add husna's code

--- src/features/landing page/components/landingPage.vue
+++ src/features/landing page/components/landingPage.vue
@@ -2,10 +2,12 @@
 import FooterComponent from "./Footer.vue";
 import HeaderComponent from "./Header.vue";
 import Component2 from "./Component2.vue";
+import Component1 from "./component1.vue";
 </script>
 
 <template>
   <HeaderComponent />
+  <Component1 />
   <Component2 />
 
   <FooterComponent />
